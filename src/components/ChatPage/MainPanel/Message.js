612b0bd8368a045186b85ad1c24bd5575c6dd06c
sangbooom <tkdqja4164@naver.com>
2020-12-31 21:58:17 +0900

    feat: 파일업로드

--- src/components/ChatPage/MainPanel/Message.js
+++ src/components/ChatPage/MainPanel/Message.js
@@ -6,37 +6,40 @@ const Message = ({ message, user }) => {
   const timeFromNow = (timestamp) => moment(timestamp).fromNow();
 
   const isImage = (message) => {
-    return message.hasOwnProperty("image") && !message.hasOwnProperty("content");
+    return (
+      message.hasOwnProperty("image") && !message.hasOwnProperty("content")
+    );
+  };
+  const isMessageMine = () => {
+    return message.user.id === user.uid
   }
 
   return (
-      <Media>
-        <img
-          style={{ borderRadius: "10px" }}
-          width={48}
-          height={48}
-          className="mr-3"
-          src={message.user.image}
-          alt={message.user.name}
-        />
-        <Media.Body>
-          <h6>
-            {message.user.name}
-            <span style={{ fontSize: "10px", color: "gray" }}>
-              {timeFromNow(message.timestamp)}
-            </span>
-          </h6>
-          {isImage(message) ? (
-            <img
-              style={{ maxWidth: "300ox" }}
-              alt="이미지"
-              src={message.image}
-            />
-          ) : (
-            <p>{message.content}</p>
-          )}
-        </Media.Body>
-      </Media>
+    <Media style={{ marginBottom: 3}}>
+      <img
+        style={{ borderRadius: "10px" }}
+        width={48}
+        height={48}
+        className="mr-3"
+        src={message.user.image}
+        alt={message.user.name}
+      />
+      <Media.Body style={{
+        backgroundColor: isMessageMine(message,user) && '#ECECEC' 
+      }}>
+        <h6>
+          {message.user.name}{" "}
+          <span style={{ fontSize: "10px", color: "gray" }}>
+            {timeFromNow(message.timestamp)}
+          </span>
+        </h6>
+        {isImage(message) ? (
+          <img style={{ maxWidth: "300ox" }} alt="이미지" src={message.image} />
+        ) : (
+          <p>{message.content}</p>
+        )}
+      </Media.Body>
+    </Media>
   );
 };
 
--- src/components/ChatPage/MainPanel/MessageForm.js
+++ src/components/ChatPage/MainPanel/MessageForm.js
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Form from "react-bootstrap/Form";
 import ProgressBar from "react-bootstrap/ProgressBar";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import firebase from "../../../firebase";
 import { useSelector } from "react-redux";
+import mime from "mime-types";
 
 const MessageForm = () => {
   const [content, setContent] = useState("");
   const [errors, setErrors] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [percentage, setPercentage] = useState(0);
   const messagesRef = firebase.database().ref("messages");
 
   const chatRoom = useSelector((state) => state.chatRoom.currentChatRoom);
   const user = useSelector((state) => state.user.currentUser);
+
+  const inputOpenImageRef = useRef();
+  const storageRef = firebase.storage().ref();
 
   const handleChange = (event) => {
     setContent(event.target.value);
@@ -59,6 +64,32 @@ const MessageForm = () => {
     }
   };
 
+  const handleOpenImageRef = () => {
+    inputOpenImageRef.current.click();
+  };
+
+  const handleUploadImage = (event) => {
+    const file = event.target.files[0];
+
+    const filePath = `/message/public/${file.name}`;
+    const metadata = { contentType: mime.lookup(file.name) };
+
+    try {
+      let uploadTask = storageRef.child(filePath).put(file, metadata);
+
+      uploadTask.on("state_changed", (UploadTaskSnapshot) => {
+        const percentage = Math.round(
+          (UploadTaskSnapshot.bytesTransferred /
+            UploadTaskSnapshot.totalBytes) *
+            100
+        );
+        setPercentage(percentage);
+      });
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <div>
       <Form onSubmit={handleSubmit}>
@@ -73,7 +104,13 @@ const MessageForm = () => {
         </Form.Group>
       </Form>
 
-      <ProgressBar variant="warning" label="60%" now={60} />
+      {!(percentage === 0 || percentage === 100) && (
+        <ProgressBar
+          variant="warning"
+          label={`${percentage}%`}
+          now={percentage}
+        />
+      )}
 
       <div>
         {errors.map((errorMsg) => (
@@ -96,7 +133,7 @@ const MessageForm = () => {
         </Col>
         <Col>
           <button
-            onClick={handleSubmit}
+            onClick={handleOpenImageRef}
             className="message-form-button"
             style={{ width: "100%" }}
             disabled={loading ? true : false}
@@ -105,6 +142,12 @@ const MessageForm = () => {
           </button>
         </Col>
       </Row>
+      <input
+        type="file"
+        style={{ display: "none" }}
+        ref={inputOpenImageRef}
+        onChange={handleUploadImage}
+      />
     </div>
   );
 };
